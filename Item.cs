4ab13d06313dcbc74dbd3f932a06b9fb57fdc7c3
adriaan2 <adriaan.kovac@gmail.com>
2023-02-20 10:14:47 +0100

    test

--- Item.cs
+++ Item.cs
@@ -1,4 +1,18 @@
 public class Item
-{
+
+   
+
     
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public Item(int id, string name, string description)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+    }
 }
+
